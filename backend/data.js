c2262a052fe33ec94b57e725f8e89c751bd552c7
sandro <aleksandre.akhvlediani.1@btu.edu.ge>
2020-07-08 19:18:22 +0400

    master

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Avengers',
+      category: 'fantasy',
+      image: '/images/1.jpg',
+      price: 60,
+      brand: ' disney',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Bohemian Rhapsody',
+      category: 'drama',
+      image: '/images/2.jpg',
+      price: 50,
+      brand: ' disney',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Amele',
+      category: 'paramount',
+      image: '/images/3.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: '1917',
+      category: 'action',
+      image: '/images/4.jpg',
+      price: 70,
+      brand: ' paramount',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+  ]
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+import CartScreen from './Screens/CartScreen';
+import SigninScreen from './Screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './Screens/RegisterScreen';
+
+function App() {
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+        </button>
+            <Link to="/" >Shop with us</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <h1>comming soon</h1>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+
+
+          </div>
+
+        </main>
+        <footer className="footer">
+          2020@
+    </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
